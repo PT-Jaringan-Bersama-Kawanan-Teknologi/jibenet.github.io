bba428bce0f1d4054a32ffdc31f82bda3824e860
jibekaat <33560777+jibekaat@users.noreply.github.com>
2025-03-05 00:35:03 +0800

    Update 12

--- assets/ios.transition-legacy-DnBCn-oR.js
+++ assets/ios.transition-legacy-DnBCn-oR.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BlLuZj5y.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-AhElUVXo.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-BFQ0JA20.js
+++ assets/swipe-back-legacy-BFQ0JA20.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BlLuZj5y.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-AhElUVXo.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
