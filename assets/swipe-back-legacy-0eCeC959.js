ea3bf215815ad890bbcdb7412bdce51afb8c1a2d
jibekaat <33560777+jibekaat@users.noreply.github.com>
2025-03-05 08:43:20 +0800

    Update 13

--- assets/swipe-back-legacy-0eCeC959.js
+++ assets/swipe-back-legacy-0eCeC959.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-AhElUVXo.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-C9lqLeW4.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
