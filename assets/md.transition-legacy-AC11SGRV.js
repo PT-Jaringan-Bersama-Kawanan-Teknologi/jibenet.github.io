7dc2a375643a2a78aef1966f7094994c4a035081
jibekaat <33560777+jibekaat@users.noreply.github.com>
2025-05-03 10:50:36 +0800

    Update 26

--- assets/md.transition-legacy-AC11SGRV.js
+++ assets/md.transition-legacy-AC11SGRV.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CfURcfPv.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-DEZe9JiG.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
