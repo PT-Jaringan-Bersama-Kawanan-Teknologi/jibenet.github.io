c24121b4c91fd167f8818a30da0c1fcfb71223b6
jibekaat <33560777+jibekaat@users.noreply.github.com>
2025-06-19 18:12:55 +0800

    Update 32

--- assets/md.transition-legacy-J-boCFdi.js
+++ assets/md.transition-legacy-J-boCFdi.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BmQAgqFd.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-DLndMj9j.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
