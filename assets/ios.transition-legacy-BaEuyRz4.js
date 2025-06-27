aa6a8dbaf60f5d45a53aa41d0dada758bba4ecb6
jibekaat <33560777+jibekaat@users.noreply.github.com>
2025-06-28 02:04:00 +0800

    Update 35

--- assets/ios.transition-legacy-BaEuyRz4.js
+++ assets/ios.transition-legacy-BaEuyRz4.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-ut_gGXc-.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-P8lZgShO.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
