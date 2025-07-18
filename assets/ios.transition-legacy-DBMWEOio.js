56f441a749e15d7ee9af3a9c9d5073995c799752
jibekaat <33560777+jibekaat@users.noreply.github.com>
2025-07-18 18:45:14 +0800

    Update 39

--- assets/ios.transition-legacy-DBMWEOio.js
+++ assets/ios.transition-legacy-DBMWEOio.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DNfG5ukr.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-BM6g2XGw.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-DGqV_KkZ.js
+++ assets/swipe-back-legacy-DGqV_KkZ.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DNfG5ukr.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-BM6g2XGw.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
