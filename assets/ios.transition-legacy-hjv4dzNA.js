a18a9b58399a7632ed7d64d2fc05890789f8307d
jibekaat <33560777+jibekaat@users.noreply.github.com>
2025-07-20 18:30:43 +0800

    Update 41

--- assets/ios.transition-legacy-hjv4dzNA.js
+++ assets/ios.transition-legacy-hjv4dzNA.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CsnOQY-g.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-DN1DmIUU.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
