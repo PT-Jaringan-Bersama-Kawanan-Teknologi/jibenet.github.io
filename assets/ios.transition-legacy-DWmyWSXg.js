e14baaa2a7f4e6239d24f4753b35e353368bb713
jibekaat <33560777+jibekaat@users.noreply.github.com>
2025-07-21 10:02:01 +0800

    Update 44

--- assets/ios.transition-legacy-DWmyWSXg.js
+++ assets/ios.transition-legacy-DWmyWSXg.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DDRRY6LK.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-wn8jYz-R.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
