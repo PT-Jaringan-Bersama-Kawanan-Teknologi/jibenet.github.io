1d8ae74bbd87e3c7eb684e89c8f5e18f6271c5cb
jibekaat <33560777+jibekaat@users.noreply.github.com>
2025-08-11 02:18:51 +0800

    Update 53

--- assets/ios.transition-legacy-B3dtjVlm.js
+++ assets/ios.transition-legacy-B3dtjVlm.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-TJ_kTI2B.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-BlaDl7vY.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
