b987fe35dd559ba58e7e8c0bbe1088bce7991c30
jibekaat <33560777+jibekaat@users.noreply.github.com>
2025-08-23 23:47:09 +0800

    Update 55

--- assets/swipe-back-legacy-BR-BTse3.js
+++ assets/swipe-back-legacy-BR-BTse3.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-b1qXKBuJ.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-Cn8933MO.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
