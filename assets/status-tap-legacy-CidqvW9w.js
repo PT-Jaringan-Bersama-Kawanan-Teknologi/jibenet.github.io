ae693d82d291c505f55d08362e792b1b5f08ae42
jibekaat <33560777+jibekaat@users.noreply.github.com>
2025-08-28 11:02:01 +0800

    Update 60

--- assets/status-tap-legacy-CidqvW9w.js
+++ assets/status-tap-legacy-CidqvW9w.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-D9Shy4Ia.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
+System.register(["./index-legacy-dcq9IO9b.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
