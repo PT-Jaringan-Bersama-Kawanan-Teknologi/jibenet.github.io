23cd6e1942488bbfe378201f8f5905c728124996
jibekaat <33560777+jibekaat@users.noreply.github.com>
2025-08-30 11:12:40 +0800

    Update 61

--- assets/ios.transition-legacy-DS9Z8uPw.js
+++ assets/ios.transition-legacy-DS9Z8uPw.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-dcq9IO9b.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-BnWsLafb.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
