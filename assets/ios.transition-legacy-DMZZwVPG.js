998782d6573c8e413111512903911dae103c629b
jibekaat <33560777+jibekaat@users.noreply.github.com>
2025-09-03 10:29:15 +0800

    Update 62

--- assets/ios.transition-legacy-DMZZwVPG.js
+++ assets/ios.transition-legacy-DMZZwVPG.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BnWsLafb.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-B_lkkZmz.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
