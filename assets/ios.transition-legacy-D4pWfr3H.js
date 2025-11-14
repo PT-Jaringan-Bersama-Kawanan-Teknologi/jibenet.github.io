4e2472e87b788677988186f513447f6edeb30daa
jibekaat <33560777+jibekaat@users.noreply.github.com>
2025-11-14 12:32:41 +0800

    Update 64

--- assets/ios.transition-legacy-D4pWfr3H.js
+++ assets/ios.transition-legacy-D4pWfr3H.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Di-PRJ2M.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-Bd64ZS1s.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
