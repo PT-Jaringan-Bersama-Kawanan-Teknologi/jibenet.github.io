90e883978eb49a3df0467de026e2959d9c2d63cd
jibekaat <33560777+jibekaat@users.noreply.github.com>
2026-02-07 21:56:53 +0800

    Update 72

--- assets/ios.transition-legacy-wi0MUioo.js
+++ assets/ios.transition-legacy-wi0MUioo.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DvBprL8y.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-BDejjuAG.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
