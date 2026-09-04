e12c5c2c286f8d393a8f8e68df1a1ee92d81c1f3
jibekaat <jilberkaat@gmail.com>
2026-09-04 17:42:16 +0800

    Update 92

--- assets/p-DIE4pXMl-legacy-Y6MGuGKF.js
+++ assets/p-DIE4pXMl-legacy-Y6MGuGKF.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BgSEzI92.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.B,i=e.r}],execute:function(){
+System.register(["./index-legacy-B2qXYpgH.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.B,i=e.r}],execute:function(){
 /*!
 			 * (C) Ionic http://ionicframework.com - MIT License
 			 */
